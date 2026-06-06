1334013cdc8f6db8b1c316c60d6734309303d1c8
Umang-Gupta-7 <umangkumargupta819144@gmail.com>
2026-06-06 12:34:27 +0530

    Day1_SumOfFirstN

---
+++ Day1/SumOfFirstN.java
@@ -0,0 +1,13 @@
+// package Summer_Assignment_25110cn276.Day1;
+import java.util.Scanner;
+public class SumOfFirstN {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a natural number=");
+        int n=sc.nextInt();
+        System.out.println("Sum of first N natural ="+((n+1)*n/2));
+        sc.close();
+    }
+    
+}
